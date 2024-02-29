d8596fe8df036ae18a19cad067d42874a5ee2533
Ripal Patel <118107470+patelripal18@users.noreply.github.com>
2024-02-29 17:55:18 +0530

    java

--- src/main/java/com/bloodbank/apis/BloodStatistics/repository/BloodStatisticsRepository.java
+++ src/main/java/com/bloodbank/apis/BloodStatistics/repository/BloodStatisticsRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
 public interface BloodStatisticsRepository extends JpaRepository<BloodStatistics,Integer>{
-//public interface BloodStatisticsRepository extends JpaRepository<BloodStatistics, Integer> {
-    // You can add custom query methods if needed
+
 }
